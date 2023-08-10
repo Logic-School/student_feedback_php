c87cf3d456e0baa2f1ef83156371074d38da9496
Rizwaan Salim <argenspin@gmail.com>
2023-08-10 12:38:15 +0530

    Added exception handling

--- submit.php
+++ submit.php
@@ -17,22 +17,30 @@
                 $purpose = $_POST["budget"];
                 $rating = $_POST["rating"];
                 $message = $_POST["message"];
-                require_once('ripcord/ripcord.php');
-                $db = "logic_testdb23";
-                $uid = "admin";
-                $password = "admin";
-                $url="http://172.19.0.3:8069";
-                $common = ripcord::client("$url/xmlrpc/2/common");
-                $uid = $common->authenticate($db, $uid, $password, array());
-                $models = ripcord::client("$url/xmlrpc/2/object");
-                $id = $models->execute_kw($db, $uid, $password, 'student.feedback', 'create', array(array("student_name"=>"$name","mobile"=>"$mobile", "counsellor"=>"$counsellor", "purpose"=>"$purpose", "rating"=>"$rating","message"=>"$message")));
-                if($id)
+                try
                 {
+                    require_once('ripcord/ripcord.php');
+                    $db = "logic_testdb23";
+                    $uid = "admin";
+                    $password = "admin";
+                    $url="http://172.19.0.3:8069";
+                    $common = ripcord::client("$url/xmlrpc/2/common");
+                    $uid = $common->authenticate($db, $uid, $password, array());
+                    $models = ripcord::client("$url/xmlrpc/2/object");
+                    $id = $models->execute_kw($db, $uid, $password, 'student.feedback', 'create', array(array("student_name"=>"$name","mobile"=>"$mobile", "counsellor"=>"$counsellor", "purpose"=>"$purpose", "rating"=>"$rating","message"=>"$message")));
                     include('successful.html');
                 }
-                else
+                catch(Exception $e)
                 {
                     include('error.html');
                 }
+                // if($id)
+                // {
+                //     include('successful.html');
+                // }
+                // else
+                // {
+                //     include('error.html');
+                // }
             }
         ?>
